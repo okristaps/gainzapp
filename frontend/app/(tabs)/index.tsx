220824a102f5268ac4e5ece26ab4446003e5d0a7
okristaps <107648450+okristaps@users.noreply.github.com>
2023-10-04 10:07:56 +0300

    route fix

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Dashboard from "../../screens/dashboard";
+import Dashboard from "../screens/dashboard";
 import Wrapper from "components/layout/wrapper";
 export default function DashboardScreen() {
   return (
